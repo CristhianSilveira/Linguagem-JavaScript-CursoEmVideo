dbb0dc262e1911a2943014b8feba93e0eb09007d
Cristhian Silveira <129897493+CristhianSilveira@users.noreply.github.com>
2023-05-10 18:12:56 -0300

    Módulo E

---
+++ AULAS/AULA 13 (REPETIÇÕES)/ex014.js
@@ -0,0 +1,19 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
+
+/*
+var c = 1
+ while (c <= 6 ){
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+
+/* OBS: c++ = (c = c + 1)
+    c < 6 conta de 0 até 5
+    c <= 6 conta de 0 até 6
+*/
